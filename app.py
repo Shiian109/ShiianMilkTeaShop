b3c01b794ec912a1937c62f653fd408da2227a86
Shiian109 <154871883+Shiian109@users.noreply.github.com>
2024-11-28 16:58:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import pandas as pd
 import streamlit as st
 import streamlit_shadcn_ui as ui
 
-df1 = pd.read('1_MilkTeaTransactions.csv')
-df2 = pd.read('2_MilkTeaOperations.csv')
-df3 = pd.read('3_MilkTeaCustomers.csv')
+df1 = pd.read_csv('1_MilkTeaTransactions.csv')
+df2 = pd.read_csv('2_MilkTeaOperations.csv')
+df3 = pd.read_csv('3_MilkTeaCustomers.csv')
 
 st.title('奶茶店交易记录')
 ui.table(df1.head(10))
