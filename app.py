ee5190e442914d23671427dc5fe337f8f26992a6
Shiian109 <154871883+Shiian109@users.noreply.github.com>
2024-11-28 17:25:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,20 @@ df1 = pd.read_csv('1_MilkTeaTransactions.csv')
 df2 = pd.read_csv('2_MilkTeaOperations.csv')
 df3 = pd.read_csv('3_MilkTeaCustomers.csv')
 
-st.title('奶茶店交易记录')
-st.dataframe(df1.head(10))
-st.markdown('---')
+analysis_type = ui.tabs(
+    options = ['奶茶店交易记录', '奶茶店运营状况', '奶茶店客户信息'],
+    default_value = '奶茶店交易记录'
+)
 
-st.title('奶茶店运营状况')
-st.dataframe(df2.head(10))
-st.markdown('---')
-
-st.title('奶茶店客户信息')
-ui.table(df3.head(10))
-st.markdown('---')
+if analysis_type == '奶茶店交易记录':  
+    st.title('奶茶店交易记录')
+    st.dataframe(df1.head(10))
+    st.markdown('---')
+elif analysis_type == '奶茶店运营状况': 
+    st.title('奶茶店运营状况')
+    st.dataframe(df2.head(10))
+    st.markdown('---')
+elif analysis_type == '奶茶店客户信息':  
+    st.title('奶茶店客户信息')
+    ui.table(df3.head(10))
+    st.markdown('---')
