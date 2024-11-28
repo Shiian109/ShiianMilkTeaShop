4fe5ba3a2a7e15d1b05ab637478b24691aa23506
Shiian109 <154871883+Shiian109@users.noreply.github.com>
2024-11-28 17:15:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ df2 = pd.read_csv('2_MilkTeaOperations.csv')
 df3 = pd.read_csv('3_MilkTeaCustomers.csv')
 
 st.title('奶茶店交易记录')
-ui.table(df1.head(10))
+st.dataframe(df1.head(10))
 st.markdown('---')
 
 st.title('奶茶店运营状况')
-ui.table(df2.head(10))
+st.dataframe(df2.head(10))
 st.markdown('---')
 
 st.title('奶茶店客户信息')
